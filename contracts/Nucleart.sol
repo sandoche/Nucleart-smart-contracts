0c362b045908046c60541362ea7b600116e462db
Sandoche Adittane <sandoche@protonmail.com>
2022-03-02 19:59:32 +0000

    :sparkles: Adding the voucher system

--- contracts/Nucleart.sol
+++ contracts/Nucleart.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Burnab
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 
 contract Nucleart is
     Initializable,
@@ -34,15 +35,92 @@ contract Nucleart is
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain. A signed voucher can be redeemed for a real NFT using the redeem function.
+    struct NFTVoucher {
+        /// @notice The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the redeem function will revert.
+        uint256 tokenId;
+        /// @notice The metadata URI to associate with this token.
+        string uri;
+        /// @notice the EIP-712 signature of all other fields in the NFTVoucher struct. For a voucher to be valid, it must be signed by an account with the MINTER_ROLE.
+        bytes signature;
+    }
+
+    /// @notice Redeems an NFTVoucher for an actual NFT, creating it in the process.
+    /// @param redeemer The address of the account which will receive the NFT upon success.
+    /// @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
+    function redeem(address redeemer, NFTVoucher calldata voucher)
+        public
+        payable
+        returns (uint256)
+    {
+        // make sure signature is valid and get the address of the signer
+        address signer = _verify(voucher);
+
+        // make sure that the signer is authorized to mint NFTs
+        require(
+            hasRole(MINTER_ROLE, signer),
+            "Signature invalid or unauthorized"
+        );
+
+        // make sure that the redeemer is paying enough to cover the buyer's cost
+        require(msg.value >= getCurrentPrice(), "Insufficient funds to redeem");
+
+        // first assign the token to the signer, to establish provenance on-chain
+        _lazyMint(signer, voucher.tokenId, voucher.uri);
+
+        // transfer the token to the redeemer
+        _transfer(signer, redeemer, voucher.tokenId);
+
+        return voucher.tokenId;
+    }
+
+    /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
+    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
+    /// @param voucher An NFTVoucher describing an unminted NFT.
+    function _verify(NFTVoucher calldata voucher)
+        internal
+        view
+        returns (address)
+    {
+        bytes32 digest = _hash(voucher);
+        return ECDSAUpgradeable.recover(digest, voucher.signature);
+    }
+
+    function getCurrentPrice() public pure returns (uint256) {
+        // todo: calculate a price based on the maximum tokens
+        return 0.001 * 10**18;
+    }
+
+    /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
+    /// @param voucher An NFTVoucher to hash.
+    function _hash(NFTVoucher calldata voucher)
+        internal
+        view
+        returns (bytes32)
+    {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "NFTVoucher(uint256 tokenId,uint256 minPrice,string uri)"
+                        ),
+                        voucher.tokenId,
+                        keccak256(bytes(voucher.uri))
+                    )
+                )
+            );
+    }
+
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://";
     }
 
-    function safeMint(
+    function _lazyMint(
         address to,
         uint256 tokenId,
         string memory uri
-    ) public onlyRole(MINTER_ROLE) {
+    ) internal {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
