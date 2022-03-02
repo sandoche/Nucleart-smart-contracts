06d4cf42d883ce75066d07e998916fe0e2a0c5a2
Sandoche Adittane <sandoche@protonmail.com>
2022-03-02 18:21:52 +0000

    :wrench: Upgrading solidity version

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
-};
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  }
+}
